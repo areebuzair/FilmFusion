9c2d04f52a2d0a1c564030900c2fc9403fba6ef7
fardeensadab <fardeensadab@gmail.com>
2025-04-29 00:48:35 +0600

    Navbar Modified

--- Frontend/src/components/MovieList.jsx
+++ Frontend/src/components/MovieList.jsx
@@ -1,20 +1,20 @@
-// Movie List component 
+// Movie List component
 // src/components/MovieList.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useAuth } from '../provider/authProvider';
-import GiveRating from './GiveRating';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth } from "../provider/authProvider";
+import GiveRating from "./GiveRating";
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
-  const {token} = useAuth()
+  const { token } = useAuth();
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const res = await axios.get('http://localhost:4500/film/movies');
+        const res = await axios.get("http://localhost:4500/film/movies");
         setMovies(res.data);
       } catch (error) {
-        console.error('Error fetching movies', error);
+        console.error("Error fetching movies", error);
       }
     };
     fetchMovies();
@@ -27,13 +27,14 @@ function MovieList() {
         <p>No movies available.</p>
       ) : (
         <ul>
-          {movies.map(movie => (
+          {movies.map((movie) => (
             <li key={movie.id}>
-              <img src={movie.poster_url} alt={movie.title}/><br/>
-              <strong>{movie.title}</strong> ({movie.release_year}) <br/>
-              Directed by {movie.director} <br/>
-              Rating: {movie.rating || 'N/A'} <br/>
-              {token && <GiveRating movie_id={movie.id}/>}
+              <img src={movie.poster_url} alt={movie.title} />
+              <br />
+              <strong>{movie.title}</strong> ({movie.release_year}) <br />
+              Directed by {movie.director} <br />
+              Rating: {movie.rating || "N/A"} <br />
+              {token && <GiveRating movie_id={movie.id} />}
             </li>
           ))}
         </ul>
