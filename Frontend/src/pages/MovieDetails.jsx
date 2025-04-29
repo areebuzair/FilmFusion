d345dedc60b7fd0f6f665345576b8ad24c8b158b
areeb <areeb@gmail.com>
2025-04-29 13:41:52 +0600

    Worked on the movie_details page

--- Frontend/src/pages/MovieDetails.jsx
+++ Frontend/src/pages/MovieDetails.jsx
@@ -7,7 +7,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 
 export default function MovieDetails() {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [movieId, setMovieId] = useState("")
+    const [movie, setMovie] = useState()
     const { token } = useAuth();
     const navigate = useNavigate()
     useEffect(() => {
@@ -15,17 +15,57 @@ export default function MovieDetails() {
         if (!id) {
             navigate("/")
         }
-        setMovieId(id)
-        console.log({ id })
+
+
+        const fetchMovies = async () => {
+            try {
+                const res = await axios.get(`http://localhost:4500/film/movies/${id}`);
+                setMovie(res.data);
+                console.log(res.data)
+            } catch (error) {
+                console.error("Error fetching movies", error);
+            }
+        };
+        fetchMovies();
+
     }, [])
     return (<>
-        <div>MovieDetails</div>
-        Movie id {movieId}
-        Average Rating
-        Reviews
-        Add to Watchlater
-        {/* Conditionally render GiveRating component */}
-        {token && <GiveRating movie_id={movieId} />}
+        {movie &&
+            <div>MovieDetails
+                <img
+                    src={movie.movie.poster_url}
+                    alt={movie.movie.title}
+                    className="movie-poster"
+                />
+                <div className="movie-info">
+                    <h3>
+                        {movie.movie.title} ({movie.movie.release_year})
+                    </h3>
+                    <p>
+                        <strong>Director:</strong> {movie.movie.director}
+                    </p>
+
+                    {/* Rating Section */}
+                    <div className="movie-rating">
+                        <span className="star">⭐</span>
+                        <span>{movie.movie.rating || "N/A"}</span>
+                    </div>
+                </div>
+                <h3>Actors</h3>
+                <ol>
+                    {movie.actors.map((actor) => (
+                        <li key={actor.id}>{actor.first_name + ' ' + actor.last_name}</li>
+                    ))}
+                </ol>
+                <h3>Genres</h3>
+                <ol>
+                    {movie.genres.map((genre) => (
+                        <li key={genre}>{genre}</li>
+                    ))}
+                </ol>
+                {token && <GiveRating movie_id={movie.movie.id} />}
+            </div>
+        }
     </>
     )
 }
